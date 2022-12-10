b750ded8644382817829f5b169f6eb370e0c10a0
Elson Oliveira <elsonfojr@gmail.com>
2022-12-10 13:11:40 -0300

    feat: endpoint to create an account created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 const express = require("express");
+const { v4: uuidV4 } = require("uuid");
 
 const app = express();
 
 app.use(express.json());
+
+const customers = [];
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement - array
+ */
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidV4(),
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
 
 app.listen(3333);
