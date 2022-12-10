b8645367958e92d52f4fb0a7ad633098166cb059
Elson Oliveira <elsonfojr@gmail.com>
2022-12-10 13:39:26 -0300

    refactor: middleware to verify account created

--- src/index.js
+++ src/index.js
@@ -7,6 +7,21 @@ app.use(express.json());
 
 const customers = [];
 
+// Middleware
+function verifyIfExistAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
 /**
  * cpf - string
  * name - string
@@ -34,15 +49,10 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
+app.use(verifyIfExistAccountCPF);
+
 app.get("/statement", (request, response) => {
-  const { cpf } = request.headers;
-
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer) {
-    return response.status(400).json({ error: "Customer not found" });
-  }
-
+  const { customer } = request;
   return response.status(200).json(customer.statement);
 });
 
